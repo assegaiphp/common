342812fe29dc593c4f065bd7ca7ca533a33d5920
Andrew Masiye <andrew.masiye@liferaftinc.com>
2023-01-22 21:21:02 +0200

    chore: configure test suite

--- src/Http/HttpClient.php
+++ src/Http/HttpClient.php
@@ -3,6 +3,7 @@
 namespace Assegai\Common\Http;
 
 use Assegai\Core\Attributes\Injectable;
+use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -13,8 +14,18 @@ readonly class HttpClient implements ClientInterface
   public function __construct(protected ClientInterface $client)
   {}
 
+  /**
+   * @param RequestInterface $request
+   * @return ResponseInterface
+   * @throws ClientExceptionInterface
+   */
   public function sendRequest(RequestInterface $request): ResponseInterface
   {
     return $this->client->sendRequest($request);
+  }
+
+  public function get(string $url, ): ResponseInterface
+  {
+
   }
 }
